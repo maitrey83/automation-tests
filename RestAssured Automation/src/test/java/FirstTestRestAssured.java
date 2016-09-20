44344484ff0d3b41547cc8a0381ebb455cf56d55
maitrey83 <maitrey@outlook.com>
2016-09-20 16:20:01 -0600

    Restful JAVA automation

---
+++ RestAssured Automation/src/test/java/FirstTestRestAssured.java
@@ -0,0 +1,18 @@
+import com.jayway.restassured.http.ContentType;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Created by maitreypatel on 9/20/2016.
+ */
+public class FirstTestRestAssured {
+
+
+
+    @Test
+    public void reserveARide()
+    {
+    }
+}
