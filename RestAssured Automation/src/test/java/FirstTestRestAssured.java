ede5de772761ef5b4de2d27ca797282159fada20
maitrey83 <maitrey@outlook.com>
2016-09-21 12:00:18 -0600

    Rest Assured Webservice sample classes

--- RestAssured Automation/src/test/java/FirstTestRestAssured.java
+++ RestAssured Automation/src/test/java/FirstTestRestAssured.java
@@ -1,18 +1,23 @@
 import com.jayway.restassured.http.ContentType;
 import org.junit.Test;
-
-import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 /**
  * Created by maitreypatel on 9/20/2016.
  */
 public class FirstTestRestAssured {
-
-
-
+    String baseUrls = "http://erex01.latte.test.ostk.com";
+    String path = "/content/directory/4";
     @Test
-    public void reserveARide()
+    public void myFirstRestAssuredTest()
     {
+        String url = baseUrls + path;
+        int statuscode = given()
+        .get(url)
+            .statusCode();
+        assertTrue(statuscode == 200);
     }
 }
