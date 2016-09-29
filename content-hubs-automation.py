e25323a994cce8b2867323abb4559161e5835b62
maitrey83 <maitrey@outlook.com>
2016-09-29 14:11:10 -0600

    Commands and content hub automation changes

--- content-hubs-automation.py
+++ content-hubs-automation.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 def siteWideRemovalsOfMetaKeywords():
     readFile = open('content-hubs-automation-urls.txt', 'r')
     lines = readFile.readlines()
@@ -10,4 +11,17 @@ def siteWideRemovalsOfMetaKeywords():
         print ("Title - " + soup.title.string)
         print("Description - " + soup.find("meta", {"name": "description"})['content'])
         print("Rel Canonicals - " +str(soup.find("link", {"rel": "canonical"})))
+
+def productListings():
+    guideUrl = 'https://www.overstock.com/guides/handheld-gps-functions-and-features'
+    guideReq = requests.get(guideUrl)
+    soupGudie = BeautifulSoup(guideReq.content, 'html.parser')
+    guides = str(soupGudie.find_all("div",{"class":"productListings"}))
+    #productUrls = str(re.findall('\"https://www.*\"', guides))
+    productUrls = str(re.findall('(https://www.+product.html)', guides))
+
+
+    print('Product Links - ' + productUrls)
+
 siteWideRemovalsOfMetaKeywords()
+productListings()
